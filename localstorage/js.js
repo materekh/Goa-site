9e45a5596710269614a36a01d905f1599ea7272e
materekh <87072420+materekh@users.noreply.github.com>
2024-01-15 18:34:50 +0400

    commit

--- localstorage/js.js
+++
@@ -1,6 +0,0 @@
-var ko=document.getElementById("ko");
-var link1=document.getElementById("link1");
-ko.addEventListener("click",function(){
-    localStorage.setItem("id=1029429374711029",form1.value);
-})
-link1.textContent=localStorage.getItem("id=1029429374711029");

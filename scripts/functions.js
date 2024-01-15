f21fe492cf3458195ef33e41f62b25c14965ad79
materekh <87072420+materekh@users.noreply.github.com>
2024-01-15 23:26:18 +0400

    commit

--- scripts/functions.js
+++ scripts/functions.js
@@ -5,4 +5,18 @@ function chlang(){
     link4.textContent="გალერეა";
     login.textContent="შესვლა";
     logout.textContent="გამოსვლა";
+    if(loginH1!=null){
+        loginH1.textContent="შესვლა";
+        nameForm.placeholder="სახელი";
+        lastNameForm.placeholder="გვარი";
+        passwordForm.placeholder="პაროლი";
+    }
+    if(stat1!=null){
+        stat1.textContent="400+ მოსწავლე";
+        stat2.textContent="10+ კურსი";
+        stat3.textContent="2k+ გამომწერი";
+        best.textContent="საუკეთესო აკადემია საქართველოში";
+        ceo.textContent="აკადემიის უფროსი";
+        ceoName.textContent="ნიკა კეშელავა";
+    }
 }

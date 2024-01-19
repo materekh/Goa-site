7440209c4fc108736788b2ac0cb52a6f43e03a32
materekh <87072420+materekh@users.noreply.github.com>
2024-01-19 19:57:39 +0400

    commit

--- images/gallery/Nothing/Nothing/Nothing/secret.js
+++ images/gallery/Nothing/Nothing/Nothing/secret.js
@@ -10,11 +10,11 @@ function sleep(i){
 body.addEventListener("click",function(){
     let tkeshel=document.getElementById("tkeshel");
     let body=document.getElementById("body");
-    tkeshel.style.animation="tkeshel1 10s,tkeshel2 10s 10s,tkeshel3 10s 20s,tkeshel4 20s 30s,tkeshel4 15s 50s,tkeshel4 10s 55s,tkeshel4 5s 65s infinite";
+    tkeshel.style.animation="tkeshel1 10s,tkeshel2 10s 10s,tkeshel3 10s 20s,tkeshel4 10s 30s,tkeshel4 5s 40s,tkeshel4 1s 45s";
     let click=document.getElementById("click");
     click.style.fontSize=0;
     click.style.display="none";
-    body.style.animation="crazy 1s linear infinite";
+    body.style.animation="crazy 0.5s linear infinite";
 }, {once : true})
 body.addEventListener("click",function(){
     let audio2=new Audio('/images/gallery/Nothing/Nothing/Nothing/programistisbedi.ogg');
